baf487f09ac625d8f5fd9e53576f3fdea77ebd56
SnowflakeTrainingAccount <nganger18@gmail.com>
2025-04-08 10:49:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(my_dataframe)
+# st.dataframe(my_dataframe)
 # st.stop()
 
 ingredients_list = st.multiselect(
